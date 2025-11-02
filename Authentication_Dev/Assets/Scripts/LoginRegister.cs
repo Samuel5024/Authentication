f49b4b615d9600111fb12747b74665f772747c3e
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2025-11-02 13:40:39 -0700

    Create PlayFabSystem game object and add the beginning of the LoginRegister.cs script

---
+++ Authentication_Dev/Assets/Scripts/LoginRegister.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+
+public class LoginRegister : MonoBehaviour
+{
+    void Start()
+    {
+        string username = "TestUser";
+        string password = "password1";
+
+        RegisterPlayFabUserRequest registerRequest = new RegisterPlayFabUserRequest
+        {
+            Username = username,
+            DisplayName = username,
+            Password = password,
+            RequireBothUsernameAndEmail = false
+
+        };
+
+        PlayFabClientAPI.RegisterPlayFabUser(registerRequest,
+            result =>
+            {
+                Debug.Log(result.PlayFabId);
+            },
+            error =>
+            {
+            Debug.Log(error);
+            }
+        );
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
